09b2100191e44d3794cf89781412eed7255538e4
ksg1103 <ksgwkd1103@naver.com>
2026-02-05 15:35:45 +0900

    쌍따옴표 3개면 입력이 편하다

---
+++ src/main/java/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String [] args){
+        Scanner sc =new Scanner("""
+                등록
+                과거에 집착하지마라.
+                작자미상
+        """); //이렇게 해서 입력을 자동화 하였다.
+
+        String str1 = sc.next();
+        String str2= sc.next();
+        String str3 = sc.next();
+        String str14= sc.next();
+
+        System.out.print(str3);
+    }
+}
