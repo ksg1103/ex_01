3476cbf916bf08662bfc239a8f3554ab0c868617
ksg1103 <ksgwkd1103@naver.com>
2026-02-05 15:46:26 +0900

    출력의 흐름 통제

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,19 +1,40 @@
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.util.Scanner;
 
 public class Main
 {
-    public static void main(String [] args){
+    public static void main(String [] args) {
+
+        PrintStream ORIGINAL_OUT  = System.out;
+
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        //출력이 byteArrayOutputStream 이곳에 쌓인다.
+        PrintStream printStream = new PrintStream(byteArrayOutputStream);
+
+        System.setOut(printStream); // 출력의 흐름 통제
+
+        System.out.println("hi");
+        System.out.println("hi");
+
+        System.setOut(ORIGINAL_OUT);
+
+        String rst = byteArrayOutputStream.toString();
+        System.out.println(rst);
+    }
+    public static void lamb(){
         Scanner sc =new Scanner("""
                 등록
-                과거에 집착하지마라.
+                과거에집착하지마라.
                 작자미상
         """); //이렇게 해서 입력을 자동화 하였다.
 
         String str1 = sc.next();
         String str2= sc.next();
         String str3 = sc.next();
-        String str14= sc.next();
 
-        System.out.print(str3);
+
+        System.out.print(str2);
     }
-}
+    }
+
