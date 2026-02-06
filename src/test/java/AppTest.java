42235bc09d785b1ba73f6f6f9e8cc031378af2d6
ksg1103 <ksgwkd1103@naver.com>
2026-02-06 12:20:11 +0900

    등록번호 노출 r

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,50 +1,43 @@
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.io.ByteArrayOutputStream;
-import java.util.Scanner;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class AppTest {
 
     @Test
-    @DisplayName("==명언 앱 출력")
-    void t1(){
-        Scanner sc = TestUtil.genScanner("종료"); //여기는 입력부분인데 입력을 자동화 시켰다.
+    @DisplayName("'== 명언 앱 ==' 출력")
+    void t1() {
+        String out = AppTestRunner.run("");
 
-        ByteArrayOutputStream outputStream = TestUtil.setOutByteArray(); //출력 통제, 왜?
-        new App(sc).run();
-
-        String out = outputStream.toString();
-
-        assertThat(out).contains("===명언 앱===");
+        assertThat(out).contains("== 명언 앱 ==");
 
     }
 
     @Test
-    @DisplayName("==명언 앱 출력")
-    void t2(){
+    @DisplayName("등록")
+    void t2() {
         String out = AppTestRunner.run("""
                 등록
                 현재를 사랑하라.
                 작자미상
                 """);
-        assertThat(out).contains("===명언 앱===");
 
+        assertThat(out).contains("명령) ");
+        assertThat(out).contains("명언 : ");
+        assertThat(out).contains("작가 : ");
     }
+
     @Test
-    @DisplayName("등록")
+    @DisplayName("등록시 명언 번호 등록 노출")
     void t3() {
         String out = AppTestRunner.run("""
                 등록
                 현재를 사랑하라.
                 작자미상
-                종료
                 """);
-        assertThat(out).contains("명령)");
-        assertThat(out).contains("명언");
-        assertThat(out).contains("작가");
-        //프로그램이 출력하는 부분만 검증 해야 한다.
+
+        assertThat(out).contains("1번 명언이 등록되었습니다.");
+
     }
 }
--- src/test/java/AppTestRunner.java
+++ src/test/java/AppTestRunner.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class AppTestRunner {
 
     public static String run(String input){
-        Scanner sc = TestUtil.genScanner(input); //여기는 입력부분인데 입력을 자동화 시켰다.
+        Scanner sc = TestUtil.genScanner(input + "\n종료"); //여기는 입력부분인데 입력을 자동화 시켰다.
 
         ByteArrayOutputStream outputStream = TestUtil.setOutByteArray(); //출력 통제, 왜?
         new App(sc).run();
