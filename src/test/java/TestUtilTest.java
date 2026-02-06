566140e263f6bb84c9d290b5b2984698a76cece2
ksg1103 <ksgwkd1103@naver.com>
2026-02-06 10:37:54 +0900

    출력 상황 R버전

--- src/test/java/TestUtilTest.java
+++ src/test/java/TestUtilTest.java
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayOutputStream;
 import java.util.Scanner;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -9,7 +10,7 @@ public class TestUtilTest {
 
     @Test
     @DisplayName("TestUtill.genScanner()")
-    void t0(){
+    void t1() {
         Scanner scanner = TestUtil.genScanner("""
                 등록
                 현재를 사랑하라.
@@ -21,21 +22,18 @@ public class TestUtilTest {
 
         assertThat(cmd).isEqualTo("등록");
     }
-//
-//    @Test
-//    @DisplayName("등록")
-//    void t1(){
-//            //클래스.함수로 바로 사용한다?(객체 생성을 안하고 사용한다?) -> static 이구나
-//            final String out = TestUtil.run("""
-//                등록
-//                현재를 사랑하라.
-//                작자미상
-//                """);
-//
-//            assertThat(out)
-//                    .contains("명언 :")
-//                    .contains("작가 :")
-//                    .contains("1번 명언이 등록되었습니다.");
-//
-//    }
+
+    @Test
+    @DisplayName("TestUtill.setOutByteArray()")
+    void t2() {
+        ByteArrayOutputStream byteArrayOutputStream = TestUtil.setOutByteArray();
+
+        System.out.println("안녕하세요.");
+
+        String rst  = byteArrayOutputStream.toString();
+
+        assertThat(rst).isEqualTo("안녕하세요");
+    }
+
+
 }
