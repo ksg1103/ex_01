54c6ee7366a1faec30c24169cc1af0ed96e017bd
ksg1103 <ksgwkd1103@naver.com>
2026-02-06 12:36:32 +0900

    순서대로 나오는거 테스트 추가

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -73,6 +73,11 @@ public class AppTest {
                 .contains("----------------------")
                 .contains("2 / 작자미상 / 과거에 집착하지 마라.")
                 .contains("1 / 작자미상 / 현재를 사랑하라.");
+        assertThat(out)
+                .contains("번호 / 작가 / 명언")
+                .contains("----------------------")
+                .containsSubsequence("2 / 작자미상 / 과거에 집착하지 마라."
+                ,"1 / 작자미상 / 현재를 사랑하라."); //시퀀스의 순서를 지켜라. 2번 1번 순서대로 나오는지 파악가능
 
     }
 }
