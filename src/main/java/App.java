8600b3cdb9af5a4b262f5964d2e6b309833b098f
ksg1103 <ksgwkd1103@naver.com>
2026-02-06 12:33:33 +0900

    목록 나오게 함

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,9 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
 
     private Scanner sc;
     int lastid;
+    List<WiseSaying> wiseSayings = new ArrayList<>();
     public App(Scanner sc) {
         this.sc  = sc;
         lastid = 0;
@@ -26,12 +29,26 @@ public class App {
 
 
                 ++lastid;
+                WiseSaying wiseSaying = new WiseSaying(lastid, saying, author);
+                wiseSayings.add(wiseSaying);
 
                 System.out.println(lastid + "번 명언이 등록되었습니다.");
             }
             case "종료" -> {
                 return;
             }
+            case "목록" -> {
+                System.out.println("번호 / 작가 / 명언");
+                System.out.println("----------------------");
+
+                wiseSayings
+                        .reversed()
+                        .stream()
+                        .forEach(wiseSaying -> System.out.printf("%d / %s / %s%n",
+                                wiseSaying.getId(), wiseSaying.getAuthor(), wiseSaying.getSaying()));
+
+            }
+
         }
     }
 
